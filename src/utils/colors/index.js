0ae7442cedb383e316164f59fbefd48312553773
RizkiAshari <rizkiashari1026@gmail.com>
2021-10-03 22:56:09 +0700

    add done colors

--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -1,12 +1,38 @@
 const mainColors = {
   blueBlack: '#2E475E',
   black: '#000000',
+  blackPudar: rgba(0, 0, 0, 0.6),
   white: '#FFFFFF',
   grey1: '#929292',
   grey2: '#F2F2F2',
   grey3: '#BEBEBE',
-  grey4: '#7C7C7C',
   red1: '#F25454',
 };
 
-export const colors = {};
+export const colors = {
+  primary: mainColors.blueBlack,
+  secondary: mainColors.grey2,
+  text: {
+    primary: mainColors.blueBlack,
+    secondary: mainColors.grey1,
+    menuActive: mainColors.red1,
+    menuInActive: mainColors.white,
+    black: mainColors.black,
+  },
+  button: {
+    primary: {
+      background: mainColors.blueBlack,
+      text: mainColors.white,
+    },
+    secondary: {
+      background: mainColors.grey2,
+      text: mainColors.grey3,
+      text2: mainColors.grey1,
+    },
+  },
+  border: mainColors.blueBlack,
+  field: {
+    background: mainColors.grey2,
+    text: mainColors.grey3,
+  },
+};
