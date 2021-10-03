25e0e2205628cd63051fa7dc0a789988e47e40a3
RizkiAshari <rizkiashari1026@gmail.com>
2021-10-03 22:27:16 +0700

    add color & fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
@@ -10,5 +10,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
---
+++ src/utils/colors/index.js
@@ -0,0 +1,12 @@
+const mainColors = {
+  blueBlack: '#2E475E',
+  black: '#000000',
+  white: '#FFFFFF',
+  grey1: '#929292',
+  grey2: '#F2F2F2',
+  grey3: '#BEBEBE',
+  grey4: '#7C7C7C',
+  red1: '#F25454',
+};
+
+export const colors = {};
